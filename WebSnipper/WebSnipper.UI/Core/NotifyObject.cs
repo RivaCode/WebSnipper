0935de662b4f706ed7b94c30f26cf949eb811494
Stas Rivkin <stasr@codevalue.net>
2017-08-08 21:02:21 +0300

    addinng RX

---
+++ WebSnipper/WebSnipper.UI/Core/NotifyObject.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WebSnipper.UI.Core
+{
+    public class NotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void SetAndNotifyPropertyChanged<T>(
+            ref T oldValue,
+            T newValue,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(oldValue, newValue)) return;
+
+            oldValue = newValue;
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
