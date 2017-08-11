4240078799e525153c8d328e682134b7f00b508f
Stas Rivkin <stasr@codevalue.net>
2017-08-12 00:31:35 +0300

    fixing uri validation

--- WebSnipper/WebSnipper.UI/Infrastructure/UrlValidator.cs
+++ WebSnipper/WebSnipper.UI/Infrastructure/UrlValidator.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Net.NetworkInformation;
+﻿using System.Net;
 using System.Threading.Tasks;
 using WebSnipper.UI.Business.Interfaces;
 
@@ -8,15 +6,6 @@ namespace WebSnipper.UI.Infrastructure
 {
     public class UrlValidator : IUrlValidator
     {
-        public async Task Validate(string url)
-        {
-            if (Uri.TryCreate(url, UriKind.Absolute, out Uri result)
-                && result.Scheme == Uri.UriSchemeHttp)
-            {
-                Ping testUrl = new Ping();
-                await testUrl.SendPingAsync(IPAddress.Parse(url));
-            }
-            throw new UriFormatException($"{url} is not a valid URL");
-        }
+        public Task Validate(string url) => WebRequest.CreateHttp(url).GetResponseAsync();
     }
 }
