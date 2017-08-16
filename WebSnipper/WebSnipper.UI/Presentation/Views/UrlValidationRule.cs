0e1cbab0c7be8c1d6aa921c0c619a354c92fa3f4
Stas Rivkin <stasr@codevalue.net>
2017-08-16 22:17:34 +0300

    updating URL validatin - WIP

---
+++ WebSnipper/WebSnipper.UI/Presentation/Views/UrlValidationRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows.Controls;
+using ValidationResult = System.Windows.Controls.ValidationResult;
+using Validator = System.Web.WebPages.Validator;
+
+namespace WebSnipper.UI.Presentation.Views
+{
+    public class UrlValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) return new ValidationResult(true, null);
+
+            if (!string.IsNullOrWhiteSpace(value as string))
+            {
+                var result = Validator.Url("Not a valid URL").Validate(new ValidationContext(value));
+                const string httpPrefix = "http://";
+
+                string uri = (string)value;
+                //if (!uri.StartsWith(httpPrefix))
+                //{
+                //    uri = $"{httpPrefix}{uri}";
+                //}
+                //if(Uri.IsWellFormedUriString(uri, UriKind.RelativeOrAbsolute))
+                    if (Uri.TryCreate(uri, UriKind.RelativeOrAbsolute, out Uri parsedUri))
+                    //    && Uri.UriSchemeHttp == parsedUri.Scheme)
+                {
+                    return new ValidationResult(true, null);
+                }
+                return new ValidationResult(false, "Not a valid URL");
+            }
+            return new ValidationResult(false, "URL field is required");
+        }
+    }
+}
