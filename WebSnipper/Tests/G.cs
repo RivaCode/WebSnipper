5547a645e6c154d9d5bd4d609d2b656fe32f145f
Stas Rivkin <stasr@codevalue.net>
2017-10-10 09:27:48 -0400

    Adding initial tests

---
+++ WebSnipper/Tests/G.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Domain;
+using Domain.Business;
+using Infrastructure;
+
+namespace Tests
+{
+    class G
+    {
+        public static void Main(string[] ar)
+        {
+            ContainerBuilder cb = new ContainerBuilder();
+            cb.RegisterModule<InfrastructureModule>();
+            cb.RegisterModule<DomainModule>();
+
+            var container = cb.Build();
+
+            container.Resolve<IGetSiteQuery>()
+                .Execute()
+                .Subscribe(x => { });
+
+            Console.Read();
+        }
+    }
+}
